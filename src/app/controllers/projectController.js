1b123e16827ffbea90c0cd821b347288dbb9cd51
Ricardo Alves Paula <>
2021-06-07 19:41:02 -0300

    CRUD em andamento

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const router = express.Router();
 const authMiddleware = require('../middlewares/auth');
 
+const Project = require('../models/Project');
+const Task = require('../models/Task');
+
+const router = express.Router();
+
 router.use(authMiddleware);
+
 
 router.get('/', (req, res) => {
     res.send({ ok: true, user: req.userId });
 });
+
+// router.get('/:projectId', async )
 
 module.exports = app => app.use('/projects', router);
